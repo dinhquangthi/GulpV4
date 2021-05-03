9316c78db5b59ffd1b951ccb67a193316107b46f
Quang Thi <dinhquangthi195@gmail.com>
2021-05-03 17:54:54 +0700

    create header responsive

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,25 @@
-console.log("Load script.js");
+function checkMenuMobile() {
+    var viewportWidth = $("body").innerWidth();
+    if (viewportWidth <= 1024) {
+        $('.nav-menu').hide()
+        $('.group-cta').hide()
+    } else {
+        $('.nav-menu').show()
+        $('.group-cta').show()
+    }
+}
+$(function () {
+    $('#open-menu').click(function () {
+        $(this).find('.fas').toggleClass('fa-times');
+        $('.nav-menu').slideToggle()
+        $('.group-cta').slideToggle()
+        $('header').toggleClass('active-menu')
+    })
+    $(window).on('resize', function () {
+        checkMenuMobile()
+    });
 
-// Instantiating the global app object
-var app = {};
+})
+$(window).on('load', function () {
+    checkMenuMobile()
+});
